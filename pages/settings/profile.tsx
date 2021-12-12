496000e7624d19e98107a2105ee967b4a0063747
Sreetam Das <sreetamdas@gmail.com>
2021-12-12 11:46:24 +0530

    wip: add slate for rich text input

--- pages/settings/profile.tsx
+++ pages/settings/profile.tsx
@@ -1,10 +1,23 @@
 import { InformationCircleIcon } from "@heroicons/react/outline";
 import crypto from "crypto";
+import isHotkey from "is-hotkey";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
-import { ComponentProps, FormEvent, RefObject, useEffect, useMemo, useRef, useState } from "react";
+import {
+  ComponentProps,
+  FormEvent,
+  RefObject,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import Select from "react-select";
 import TimezoneSelect, { ITimezone } from "react-timezone-select";
+import { Editor, Transforms, createEditor, Descendant, Element as SlateElement } from "slate";
+import { withHistory } from "slate-history";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
 
 import { QueryCell } from "@lib/QueryCell";
 import { asStringOrNull, asStringOrUndefined } from "@lib/asStringOrNull";
@@ -91,6 +104,120 @@ function HideBrandingInput(props: { hideBrandingRef: RefObject<HTMLInputElement>
     </>
   );
 }
+
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && LIST_TYPES.includes(n.type),
+    split: true,
+  });
+  const newProperties: Partial<SlateElement> = {
+    type: isActive ? "paragraph" : isList ? "list-item" : format,
+  };
+  Transforms.setNodes<SlateElement>(editor, newProperties);
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
+  }
+};
+
+// const toggleMark = (editor, format) => {
+//   const isActive = isMarkActive(editor, format);
+
+//   if (isActive) {
+//     Editor.removeMark(editor, format);
+//   } else {
+//     Editor.addMark(editor, format, true);
+//   }
+// };
+
+const isBlockActive = (editor, format) => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Editor.nodes(editor, {
+    at: Editor.unhangRange(editor, selection),
+    match: (n) => !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format,
+  });
+
+  return !!match;
+};
+
+// const isMarkActive = (editor, format) => {
+//   const marks = Editor.marks(editor);
+//   return marks ? marks[format] === true : false;
+// };
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
+
+// const BlockButton = ({ format, icon }) => {
+//   const editor = useSlate();
+//   return (
+//     <Button
+//       active={isBlockActive(editor, format)}
+//       onMouseDown={(event) => {
+//         event.preventDefault();
+//         toggleBlock(editor, format);
+//       }}>
+//       <Icon>{icon}</Icon>
+//     </Button>
+//   );
+// };
+
+// const MarkButton = ({ format, icon }) => {
+//   const editor = useSlate();
+//   return (
+//     <Button
+//       active={isMarkActive(editor, format)}
+//       onMouseDown={(event) => {
+//         event.preventDefault();
+//         toggleMark(editor, format);
+//       }}>
+//       <Icon>{icon}</Icon>
+//     </Button>
+//   );
+// };
 
 function SettingsView(props: ComponentProps<typeof Settings> & { localeProp: string }) {
   const utils = trpc.useContext();
@@ -183,6 +310,11 @@ function SettingsView(props: ComponentProps<typeof Settings> & { localeProp: str
     });
   }
 
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
   return (
     <form className="divide-y divide-gray-200 lg:col-span-9" onSubmit={updateProfileHandler}>
       {hasErrors && <Alert severity="error" title={errorMessage} />}
@@ -257,6 +389,35 @@ function SettingsView(props: ComponentProps<typeof Settings> & { localeProp: str
                   defaultValue={props.user.bio || undefined}
                   className="block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:ring-neutral-800 focus:border-neutral-800 sm:text-sm"></textarea>
               </div>
+              <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
+                {/* <Toolbar>
+                  <MarkButton format="bold" icon="format_bold" />
+                  <MarkButton format="italic" icon="format_italic" />
+                  <MarkButton format="underline" icon="format_underlined" />
+                  <MarkButton format="code" icon="code" />
+                  <BlockButton format="heading-one" icon="looks_one" />
+                  <BlockButton format="heading-two" icon="looks_two" />
+                  <BlockButton format="block-quote" icon="format_quote" />
+                  <BlockButton format="numbered-list" icon="format_list_numbered" />
+                  <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+                </Toolbar> */}
+                <Editable
+                  renderElement={renderElement}
+                  renderLeaf={renderLeaf}
+                  placeholder="Enter some rich text…"
+                  spellCheck
+                  autoFocus
+                  // onKeyDown={(event) => {
+                  //   for (const hotkey in HOTKEYS) {
+                  //     if (isHotkey(hotkey, event as any)) {
+                  //       event.preventDefault();
+                  //       const mark = HOTKEYS[hotkey];
+                  //       toggleMark(editor, mark);
+                  //     }
+                  //   }
+                  // }}
+                />
+              </Slate>
             </div>
             <div>
               <div className="flex mt-1">
@@ -476,3 +637,18 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
     },
   };
 };
+
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "This is editable " },
+      { text: "rich", bold: true },
+      { text: " text, " },
+      { text: "much", italic: true },
+      { text: " better than a " },
+      { text: "<textarea>", code: true },
+      { text: "!" },
+    ],
+  },
+];
